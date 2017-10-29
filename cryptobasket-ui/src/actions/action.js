fb33c540f4c4c3b6adb5598842144af0dbca7315
lixi2020 <eko.lee@outlook.sg>
2017-10-29 15:38:41 +0800

    Add action & reducer

---
+++ cryptobasket-ui/src/actions/action.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const FETCH_TICKER_PENDING='fetch_tickers_pending';
+export const FETCH_TICKER_FULFILLED='fetch_tickers_fulfilled';
+
+
+const APPSERVER_URL='';
+
+export function fetchTickers(){
+    const request=
+}
